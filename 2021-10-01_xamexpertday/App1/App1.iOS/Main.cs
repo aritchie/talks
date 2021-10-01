00e7538fd195ef0d3032eda386e7afbd2bc28cfe
Allan Ritchie <allan.ritchie@gmail.com>
2021-09-30 21:54:57 -0400

    xamexpertday

---
+++ 2021-10-01_xamexpertday/App1/App1.iOS/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UIKit;
+
+
+namespace App1.iOS
+{
+    public class Application
+    {
+        static void Main(string[] args)
+            => UIApplication.Main(args, null, "AppDelegate");
+    }
+}
---
+++ 2021-10-01_xamexpertday/App1/App1/MyShinyStartup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+using Microsoft.Extensions.Logging;
+
+
+namespace App1
+{
+    public class MyShinyStartup : ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection services, IPlatform platform)
+        {
+            services.RegisterJob(new Shiny.Jobs.JobInfo(typeof(MyJob)) {
+                DeviceCharging = true,
+                BatteryNotLow = true,
+                RequiredInternetAccess = Shiny.Jobs.InternetAccess.Any
+            });
+            services.UseNotifications();
+
+            //services.UsePush<MyPushDelegate>();
+            //services.UsePushAzureNotificationHubs<MyPushDelegate>("Listener connection string", "hub name");
+        }
+
+
+        public override void ConfigureLogging(ILoggingBuilder builder, IPlatform platform)
+        {
+            builder.AddAppCenter("AppCenterSecret");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+////services.UsePush<MyPushDelegate>();
+//services.UsePushAzureNotificationHubs<MyPushDelegate>("", "");
